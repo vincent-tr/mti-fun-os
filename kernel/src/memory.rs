52fa5762a2866f0dcaa801c9c8f28952457a3889
vincent-tr <vincent@stoic.com>
2021-08-12 22:19:06 +0200

    wip frame allocator

--- kernel/src/memory.rs
+++ kernel/src/memory.rs
@@ -1,7 +1,7 @@
 use bootloader::BootInfo;
 use x86_64::{
-    structures::paging::{mapper::OffsetPageTable, PageSize, PageTable, Size4KiB},
-    VirtAddr,
+    structures::paging::{mapper::OffsetPageTable, PageSize, PageTable, PhysFrame, Size4KiB},
+    PhysAddr, VirtAddr,
 };
 
 use crate::println;
@@ -45,4 +45,34 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr // unsafe
+}
+
+// https://wiki.osdev.org/Page_Frame_Allocation
+pub struct FrameAllocator<'a> {
+    bitmap: &'a mut [u64],
+}
+
+impl<'a> FrameAllocator<'a> {
+    pub fn allocate(&mut self) -> PhysFrame {
+        for (word_index, word) in self.bitmap.iter_mut().enumerate() {
+            let bit_index = word.leading_ones() as usize;
+            if bit_index < 64 {
+                *word |= 1u64 << bit_index;
+
+                let page_offset = word_index * 64 + bit_index;
+                let address = PhysAddr::new((page_offset * PAGE_SIZE) as u64);
+                return PhysFrame::from_start_address(address).unwrap();
+            }
+        }
+
+        panic!("Frame allocator: allocation failure, no more free frame");
+    }
+
+    pub fn deallocate(&mut self, frame: PhysFrame) {
+        let page_offset = frame.start_address().as_u64() as usize / PAGE_SIZE;
+        let word_index = page_offset / 64;
+        let bit_index = (page_offset - (word_index * 64)) as usize;
+        let word = unsafe { self.bitmap.get_unchecked_mut(word_index) };
+        *word &= !(1u64 << bit_index);
+    }
 }

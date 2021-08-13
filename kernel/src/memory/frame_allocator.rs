d5cd789046ccfa6c473e304f9a33f0e2e6c1c181
vincent-tr <vincent@stoic.com>
2021-08-13 17:07:25 +0200

    wip memory

--- kernel/src/memory/frame_allocator.rs
+++ kernel/src/memory/frame_allocator.rs
@@ -1,36 +1,34 @@
-use x86_64::{
-  structures::paging::PhysFrame,
-  PhysAddr,
-};
+use x86_64::{structures::paging::PhysFrame, PhysAddr};
 
+use crate::memory::PAGE_SIZE;
 
 // https://wiki.osdev.org/Page_Frame_Allocation
 
 pub struct FrameAllocator<'a> {
-  bitmap: &'a mut [u64],
+    bitmap: &'a mut [u64],
 }
 
 impl<'a> FrameAllocator<'a> {
-  pub fn allocate(&mut self) -> PhysFrame {
-      for (word_index, word) in self.bitmap.iter_mut().enumerate() {
-          let bit_index = word.leading_ones() as usize;
-          if bit_index < 64 {
-              *word |= 1u64 << bit_index;
+    pub fn allocate(&mut self) -> PhysFrame {
+        for (word_index, word) in self.bitmap.iter_mut().enumerate() {
+            let bit_index = word.leading_ones() as usize;
+            if bit_index < 64 {
+                *word |= 1u64 << bit_index;
 
-              let page_offset = word_index * 64 + bit_index;
-              let address = PhysAddr::new((page_offset * PAGE_SIZE) as u64);
-              return PhysFrame::from_start_address(address).unwrap();
-          }
-      }
+                let page_offset = word_index * 64 + bit_index;
+                let address = PhysAddr::new((page_offset * PAGE_SIZE) as u64);
+                return PhysFrame::from_start_address(address).unwrap();
+            }
+        }
 
-      panic!("Frame allocator: allocation failure, no more free frame");
-  }
+        panic!("Frame allocator: allocation failure, no more free frame");
+    }
 
-  pub fn deallocate(&mut self, frame: PhysFrame) {
-      let page_offset = frame.start_address().as_u64() as usize / PAGE_SIZE;
-      let word_index = page_offset / 64;
-      let bit_index = (page_offset - (word_index * 64)) as usize;
-      let word = unsafe { self.bitmap.get_unchecked_mut(word_index) };
-      *word &= !(1u64 << bit_index);
-  }
+    pub fn deallocate(&mut self, frame: PhysFrame) {
+        let page_offset = frame.start_address().as_u64() as usize / PAGE_SIZE;
+        let word_index = page_offset / 64;
+        let bit_index = (page_offset - (word_index * 64)) as usize;
+        let word = unsafe { self.bitmap.get_unchecked_mut(word_index) };
+        *word &= !(1u64 << bit_index);
+    }
 }
--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -9,21 +9,16 @@ use crate::println;
 pub const PAGE_SIZE: usize = Size4KiB::SIZE as usize;
 pub const KERNEL_STACK_SIZE: usize = PAGE_SIZE * 5;
 
-/// `Offseted` mapping of physical memory starting at this offset in virtual memory
-static mut physical_memory_offset: u64 = 0;
+mod frame_allocator;
+mod paging;
+
+pub use paging::{to_phys, to_virt_view};
 
 // WARNING: this is static, so only valid until physical_memory_offset changes, or cr3 changes
 static mut MAPPER: Option<OffsetPageTable> = Option::None;
 
 pub fn init(boot_info: &'static BootInfo) {
-    println!(
-        "Physical memory offset: {:#X}",
-        boot_info.physical_memory_offset
-    );
-
-    unsafe {
-        physical_memory_offset = boot_info.physical_memory_offset;
-    }
+    paging::init(boot_info);
 
     for region in boot_info.memory_map.iter() {
         println!("Region: {:?} {:?}", region.region_type, region.range);
@@ -31,16 +26,11 @@ pub fn init(boot_info: &'static BootInfo) {
 
     unsafe {
         let l4_page_table = active_level_4_table();
-        MAPPER = Some(OffsetPageTable::new(l4_page_table, VirtAddr::new(physical_memory_offset)));
+        MAPPER = Some(OffsetPageTable::new(
+            l4_page_table,
+            VirtAddr::new(physical_memory_offset),
+        ));
     }
-}
-
-pub fn to_phys(virt: VirtAddr) -> PhysAddr {
-    return PhysAddr::new(virt.as_u64() - unsafe { physical_memory_offset });
-}
-
-pub fn to_virt_view(phys: PhysAddr) -> VirtAddr {
-    return VirtAddr::new(phys.as_u64() + unsafe { physical_memory_offset });
 }
 
 fn mapper() -> &'static OffsetPageTable<'static> {

e4d2c991866f3d265c3de7230293b5c9868a66d6
vincent-tr <vincent@stoic.com>
2021-08-16 22:37:49 +0200

    frame allocator

--- kernel/src/memory/frame_allocator.rs
+++ kernel/src/memory/frame_allocator.rs
@@ -1,10 +1,9 @@
 use bootloader::bootinfo::{BootInfo, MemoryRegionType};
 use core::{mem::size_of, slice};
 use spin::Mutex;
-use x86_64::{structures::paging::PhysFrame, PhysAddr, VirtAddr, align_up};
+use x86_64::{align_up, structures::paging::PhysFrame, PhysAddr, VirtAddr};
 
-use crate::println;
-use crate::{error::Error, memory::PAGE_SIZE};
+use crate::{error::Error, memory::PAGE_SIZE, println};
 
 // https://wiki.osdev.org/Page_Frame_Allocation
 
@@ -69,15 +68,17 @@ impl<'a> FrameAllocator<'a> {
             if self.stack.len() == 0 {
                 // We must init the stack, using part of this first free range.
                 let total_frames = physical_memory_size as usize / PAGE_SIZE;
-                let needed_frames = align_up((total_frames * size_of::<u32>()) as u64, PAGE_SIZE as u64) / PAGE_SIZE as u64;
+                let needed_frames =
+                    align_up((total_frames * size_of::<u32>()) as u64, PAGE_SIZE as u64)
+                        / PAGE_SIZE as u64;
 
                 // Assert the range is big enough
                 assert!(range.start_frame_number + needed_frames <= range.end_frame_number);
 
                 let address = to_virt_view(PhysAddr::new(range.start_addr()));
-                
+
                 self.stack =
-                unsafe { slice::from_raw_parts_mut(address.as_mut_ptr(), total_frames) };
+                    unsafe { slice::from_raw_parts_mut(address.as_mut_ptr(), total_frames) };
                 self.stack.fill(0);
 
                 let start_frame_number = (range.start_frame_number + needed_frames) as u32;
@@ -92,11 +93,14 @@ impl<'a> FrameAllocator<'a> {
             }
         }
 
-        println!("Frame allocator: {} total frames, {} free", self.stack.len(), self.top);
+        println!(
+            "Frame allocator: {} total frames, {} free",
+            self.stack.len(),
+            self.top
+        );
     }
 
     fn add_region(&mut self, start_frame_number: u32, end_frame_number: u32) {
-        println!("Add region {} {}", start_frame_number, end_frame_number);
         for frame_number in start_frame_number..end_frame_number {
             assert!(self.top < self.stack.len());
             self.stack[self.top] = frame_number;
@@ -124,13 +128,11 @@ impl<'a> FrameAllocator<'a> {
     }
 
     fn frame_to_frame_number(frame: PhysFrame) -> u32 {
-        return (frame.start_address().as_u64() as usize / PAGE_SIZE) as u32;
+        (frame.start_address().as_u64() as usize / PAGE_SIZE) as u32
     }
 
     fn frame_number_to_frame(frame_number: u32) -> PhysFrame {
-        return PhysFrame::from_start_address(PhysAddr::new(
-            frame_number as u64 * PAGE_SIZE as u64,
-        ))
-        .unwrap();
+        let phys_addr = PhysAddr::new(frame_number as u64 * PAGE_SIZE as u64);
+        PhysFrame::from_start_address(phys_addr).unwrap()
     }
 }

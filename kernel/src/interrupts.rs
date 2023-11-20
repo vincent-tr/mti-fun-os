58cf4d844f2565ca84c59ba78a000420952188a8
vincent-tr <vincent.tr@gmail.com>
2023-11-20 18:35:21 +0100

    wip interrupts/gdt

---
+++ kernel/src/interrupts.rs
@@ -0,0 +1,26 @@
+use crate::gdt;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
+                .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
+        }
+        idt
+    };
+}
+
+pub fn init_idt() {
+    IDT.load();
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
 
 extern crate bootloader_api;
 extern crate lazy_static;
 
+mod gdt;
+mod interrupts;
 mod logging;
 
 use bootloader_api::info::MemoryRegionKind;
@@ -18,6 +21,14 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
 
     info!("Entered kernel with boot info: {boot_info:?}");
 
+    print_mem(&boot_info);
+
+    gdt::init();
+    interrupts::init_idt();
+
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    };
 
     panic!("End of main!");
 }
@@ -26,4 +37,39 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
 fn panic(info: &PanicInfo) -> ! {
     error!("PANIC: {info}");
     loop {}
+}
+
+fn print_mem(boot_info: &BootInfo) {
+    for region in boot_info.memory_regions.iter() {
+        info!("region: {region:?}");
+    }
+
+    let mut start: u64 = 0;
+    let mut size: u64 = 0;
+
+    for region in boot_info.memory_regions.iter() {
+        if let MemoryRegionKind::Usable = region.kind {
+            if start > 0 && start + size == region.start {
+                size += region.end - region.start;
+            } else {
+                if size > 0 {
+                    info!("usable region: start={start}, size={size}");
+                }
+
+                start = region.start;
+                size = region.end - region.start;
+            }
+        } else {
+            if size > 0 {
+                info!("usable region: start={start}, size={size}");
+            }
+
+            start = 0;
+            size = 0;
+        }
+    }
+
+    if size > 0 {
+        info!("usable region: start={start}, size={size}");
+    }
 }

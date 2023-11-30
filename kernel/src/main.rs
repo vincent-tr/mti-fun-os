791634cd5507e86f0b691f289e83af463abcba13
vincent-tr <vincent.tr@gmail.com>
2023-11-30 10:23:53 +0100

    test paging

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -16,9 +16,11 @@ mod logging;
 mod memory;
 
 use bootloader_api::{config::Mapping, entry_point, BootInfo, BootloaderConfig};
-use x86_64::VirtAddr;
 use core::panic::PanicInfo;
 use log::{error, info};
+use x86_64::VirtAddr;
+
+use crate::memory::PAGE_SIZE;
 
 const CONFIG: BootloaderConfig = {
     let mut config = BootloaderConfig::new_default();
@@ -33,7 +35,12 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     logging::init();
 
     let version = &boot_info.api_version;
-    info!("Starting kernel with boot info v{}.{}.{}", version.version_major(), version.version_minor(), version.version_patch());
+    info!(
+        "Starting kernel with boot info v{}.{}.{}",
+        version.version_major(),
+        version.version_minor(),
+        version.version_patch()
+    );
 
     let physical_memory_offset = VirtAddr::new(*boot_info.physical_memory_offset.as_ref().unwrap());
 
@@ -41,6 +48,54 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     interrupts::init_idt();
     memory::phys::init(physical_memory_offset, &boot_info.memory_regions);
     memory::paging::init(physical_memory_offset);
+
+    let free_before = memory::phys::stats().free;
+
+    let mut frame = memory::phys::allocate().unwrap();
+    let addr1 = memory::VMALLOC_START;
+    let addr2 = memory::VMALLOC_START + PAGE_SIZE * 3;
+
+    unsafe {
+        let mut frame = frame.clone();
+
+        memory::paging::KERNEL_ADDRESS_SPACE
+            .map(
+                addr1,
+                &mut frame,
+                memory::paging::Permissions::READ | memory::paging::Permissions::WRITE,
+            )
+            .unwrap();
+
+        let data: *mut i64 = addr1.as_mut_ptr();
+        *data = 42;
+
+        memory::paging::KERNEL_ADDRESS_SPACE.unmap(addr1).unwrap();
+    }
+
+    // map frame somewhere else
+    unsafe {
+        let mut frame = frame.clone();
+
+        memory::paging::KERNEL_ADDRESS_SPACE
+            .map(
+                addr2,
+                &mut frame,
+                memory::paging::Permissions::READ | memory::paging::Permissions::WRITE,
+            )
+            .unwrap();
+
+            let data: *mut i64 = addr2.as_mut_ptr();
+
+            info!("data={}", *data);
+
+        memory::paging::KERNEL_ADDRESS_SPACE.unmap(addr2).unwrap();
+    }
+
+    core::mem::drop(frame);
+
+    let free_after = memory::phys::stats().free;
+
+    info!("free: before={free_before}, after={free_after}, eq={}", free_before == free_after);
 
     // Note:
     // boot_info is unmapped from here.

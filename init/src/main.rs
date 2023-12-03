84c5538cd95e6d23d3c29e979ec22ea712485d8d
vincent-tr <vincent.tr@gmail.com>
2023-12-03 21:46:06 +0100

    Add syscalls

--- init/src/main.rs
+++ init/src/main.rs
@@ -1,10 +1,19 @@
 #![no_std]
 #![no_main]
 
+mod syscalls;
+
 use core::panic::PanicInfo;
+
+use syscalls::syscall0;
+
+const SYSCALL_EXIT: usize = 1;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    unsafe {
+        syscall0(SYSCALL_EXIT);
+    }
     loop {}
 }
 

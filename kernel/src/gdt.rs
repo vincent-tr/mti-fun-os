ec19cd85bcba93b19efbb101a6f964337dee7fad
vincent-tr <vincent.tr@gmail.com>
2023-12-12 10:30:28 +0100

    Better kernel stack + add userland segments

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -1,20 +1,21 @@
+use crate::memory::KernelStack;
 use lazy_static::lazy_static;
 use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector};
 use x86_64::structures::tss::TaskStateSegment;
-use x86_64::VirtAddr;
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
+
+pub const KERNEL_CODE_SELECTOR_INDEX: u16 = 1;
+pub const KERNEL_DATA_SELECTOR_INDEX: u16 = 2;
+pub const USER_DATA_SELECTOR_INDEX: u16 = 3; // Note: to configure STAR syscall register properly
+pub const USER_CODE_SELECTOR_INDEX: u16 = 4;
 
 lazy_static! {
     static ref TSS: TaskStateSegment = {
         let mut tss = TaskStateSegment::new();
         tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
-            const STACK_SIZE: usize = 4096 * 5;
-            static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
-
-            let stack_start = VirtAddr::from_ptr(unsafe { &STACK });
-            let stack_end = stack_start + STACK_SIZE;
-            stack_end
+            static mut STACK: KernelStack = KernelStack::new();
+            unsafe { STACK.stack_top() }
         };
         tss
     };
@@ -23,20 +24,28 @@ lazy_static! {
 lazy_static! {
     static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
-        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
-        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
-        (
-            gdt,
-            Selectors {
-                code_selector,
-                tss_selector,
-            },
-        )
+        let selectors = Selectors {
+            kernel_code_selector: gdt.add_entry(Descriptor::kernel_code_segment()),
+            kernel_data_selector: gdt.add_entry(Descriptor::kernel_data_segment()),
+            user_data_selector: gdt.add_entry(Descriptor::user_data_segment()),
+            user_code_selector: gdt.add_entry(Descriptor::user_code_segment()),
+            tss_selector: gdt.add_entry(Descriptor::tss_segment(&TSS)),
+        };
+
+        assert!(selectors.kernel_code_selector.index() == KERNEL_CODE_SELECTOR_INDEX);
+        assert!(selectors.kernel_data_selector.index() == KERNEL_DATA_SELECTOR_INDEX);
+        assert!(selectors.user_code_selector.index() == USER_CODE_SELECTOR_INDEX);
+        assert!(selectors.user_data_selector.index() == USER_DATA_SELECTOR_INDEX);
+
+        (gdt, selectors)
     };
 }
 
 struct Selectors {
-    code_selector: SegmentSelector,
+    kernel_code_selector: SegmentSelector,
+    kernel_data_selector: SegmentSelector,
+    user_code_selector: SegmentSelector,
+    user_data_selector: SegmentSelector,
     tss_selector: SegmentSelector,
 }
 
@@ -46,7 +55,23 @@ pub fn init() {
 
     GDT.0.load();
     unsafe {
-        CS::set_reg(GDT.1.code_selector);
+        CS::set_reg(GDT.1.kernel_code_selector);
         load_tss(GDT.1.tss_selector);
     }
+}
+
+pub fn kernel_code_selector() -> SegmentSelector {
+    GDT.1.kernel_code_selector
+}
+
+pub fn kernel_data_selector() -> SegmentSelector {
+    GDT.1.kernel_data_selector
+}
+
+pub fn user_code_selector() -> SegmentSelector {
+    GDT.1.user_code_selector
+}
+
+pub fn user_data_selector() -> SegmentSelector {
+    GDT.1.user_data_selector
 }

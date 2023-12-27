10bb5d60bc0e2a163314c8e7ea0cad5365063f83
vincent-tr <vincent.tr@gmail.com>
2023-12-27 21:49:30 +0100

    move number at root

--- syscalls/src/lib.rs
+++ syscalls/src/lib.rs
@@ -1,11 +1,30 @@
 #![no_std]
 
 mod error;
-mod number;
 mod permissions;
 mod thread_priority;
 
 pub use error::{Error, SUCCESS};
-pub use number::SyscallNumber;
 pub use permissions::Permissions;
 pub use thread_priority::ThreadPriority;
+
+/// List of syscall numbers
+#[repr(usize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum SyscallNumber {
+    Log = 1,
+    Close,
+    Duplicate,
+    ProcessOpenSelf,
+    ProcessCreate,
+    ProcessMMap,
+    ProcessMUnmap,
+    ProcessMProtect,
+    ThreadOpenSelf,
+    ThreadCreate,
+    ThreadExit,
+    ThreadKill,
+    ThreadSetPriority,
+
+    InitSetup,
+}

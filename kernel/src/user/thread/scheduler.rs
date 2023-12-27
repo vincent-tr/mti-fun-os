5e4e37d07a2b25fbdddd5eab6d661950b5b3a8d1
vincent-tr <vincent.tr@gmail.com>
2023-12-27 21:15:49 +0100

    thread priority in scheduler

--- kernel/src/user/thread/scheduler.rs
+++ kernel/src/user/thread/scheduler.rs
@@ -2,6 +2,7 @@ use lazy_static::lazy_static;
 
 use alloc::{collections::LinkedList, sync::Arc};
 use spin::RwLock;
+use syscalls::ThreadPriority;
 
 use super::Thread;
 
@@ -11,15 +12,27 @@ lazy_static! {
 
 #[derive(Debug)]
 pub struct Scheduler {
-    // TODO: Very basic for now
-    ready_list: RwLock<LinkedList<Arc<Thread>>>,
+    ready_list: RwLock<[LinkedList<Arc<Thread>>; 7]>,
 }
 
 impl Scheduler {
     fn new() -> Self {
         Self {
-            ready_list: RwLock::new(LinkedList::new()),
+            ready_list: RwLock::new([
+                LinkedList::new(),
+                LinkedList::new(),
+                LinkedList::new(),
+                LinkedList::new(),
+                LinkedList::new(),
+                LinkedList::new(),
+                LinkedList::new(),
+            ]),
         }
+    }
+
+    const fn index(priority: ThreadPriority) -> usize {
+        // Note: reverse so that array starts with hightest priority
+        ThreadPriority::TimeCritical as usize - priority as usize
     }
 
     /// Add a new thread to the ready list
@@ -27,19 +40,21 @@ impl Scheduler {
         assert!(thread.state().is_ready());
 
         let mut ready_list = self.ready_list.write();
-        ready_list.push_back(thread);
+        ready_list[Self::index(thread.priority())].push_back(thread);
     }
 
     /// Remove a thread from the ready list
     pub fn remove(&self, thread: &Arc<Thread>) {
         let mut ready_list = self.ready_list.write();
 
-        // TODO: very inefficient
-        let mut cursor = ready_list.cursor_back_mut();
-        while let Some(item) = cursor.current() {
-            if Arc::ptr_eq(item, &thread) {
-                cursor.remove_current();
-                return;
+        // Note: very inefficient
+        for list in ready_list.iter_mut() {
+            let mut cursor = list.cursor_back_mut();
+            while let Some(item) = cursor.current() {
+                if Arc::ptr_eq(item, &thread) {
+                    cursor.remove_current();
+                    return;
+                }
             }
         }
 
@@ -50,6 +65,14 @@ impl Scheduler {
     /// Decide which thread should be next executed, and pop it out of the ready list
     pub fn schedule(&self) -> Arc<Thread> {
         let mut ready_list = self.ready_list.write();
-        ready_list.pop_front().expect("Ready list empty !")
+
+        // Hightest priority first
+        for list in ready_list.iter_mut() {
+            if let Some(thread) = list.pop_front() {
+                return thread;
+            }
+        }
+
+        panic!("Ready list empty !");
     }
 }

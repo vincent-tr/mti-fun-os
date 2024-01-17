b32834514486167d5fa404285cbbfb47bd4780b0
vincent-tr <vincent.tr@gmail.com>
2024-01-17 17:09:30 +0100

    vfs-server skeleton

---
+++ vfs-server/src/main.rs
@@ -0,0 +1,11 @@
+#![no_std]
+#![no_main]
+#![feature(naked_functions)]
+#![feature(used_with_arg)]
+
+extern crate alloc;
+extern crate libruntime;
+
+fn main() {
+    // println!("Hello, world!");
+}

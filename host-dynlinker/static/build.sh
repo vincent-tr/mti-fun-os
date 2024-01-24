35e5a951b4fcbb9baea52efb88e4c56ba01f831e
vincent-tr <vincent.tr@gmail.com>
2024-01-24 09:51:19 +0100

    more testing binary

--- host-dynlinker/static/build.sh
+++ host-dynlinker/static/build.sh
@@ -4,4 +4,13 @@
 # go build hello.go
 
 # static PIE
-go build -ldflags '-linkmode external -s -w -extldflags "--static-pie"' -buildmode=pie hello.go
+# go build -ldflags '-linkmode external -s -w -extldflags "--static-pie"' -buildmode=pie hello.go
+
+# static EXEC
+gcc -static -o hello hello.c
+
+# no libc static
+# gcc -static -nostdlib -Wno-builtin-declaration-mismatch -o hello hello_nolibc.c
+
+# no libc PIE
+# gcc -nostdlib -Wno-builtin-declaration-mismatch -o hello hello_nolibc.c

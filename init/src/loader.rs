16c65f5441834c37fc0606b4d719832c7bd1c5fd
vincent-tr <vincent.tr@gmail.com>
2026-01-24 11:26:20 +0100

    wip loader

--- init/src/loader.rs
+++ init/src/loader.rs
@@ -1,100 +1,92 @@
-use core::{error::Error, fmt};
+use core::{error::Error, fmt, panic};
+use libruntime::kobject;
 use log::debug;
-use xmas_elf::{
-    header, program,
-    sections::{SectionData, ShType},
-    ElfFile,
-};
+use xmas_elf::{header, program, ElfFile};
 
 // Very simple loader.
-// It must load libruntime.so, then load process-server, which can only depends on libruntime.so.
-
+// It only loads static binaries for now, with no checks
 pub fn load(binary: &[u8]) -> Result<(), LoaderError> {
-    let elf_file = wrap_res(xmas_elf::ElfFile::new(binary))?;
+    let elf_file = xmas_elf::ElfFile::new(binary)?;
 
     for program_header in elf_file.program_iter() {
-        wrap_res(program::sanity_check(program_header, &elf_file))?;
+        program::sanity_check(program_header, &elf_file)?;
     }
 
     match elf_file.header.pt2.type_().as_type() {
         header::Type::None => Err(LoaderError::BadObjectType("none"))?,
         header::Type::Relocatable => Err(LoaderError::BadObjectType("relocatable"))?,
-        header::Type::Executable => Err(LoaderError::BadObjectType("executable"))?,
-        header::Type::SharedObject => (),
+        header::Type::Executable => (),
+        header::Type::SharedObject => Err(LoaderError::BadObjectType("shared-object"))?,
         header::Type::Core => Err(LoaderError::BadObjectType("core"))?,
         header::Type::ProcessorSpecific(_) => {
             Err(LoaderError::BadObjectType("processor-specific"))?
         }
     };
 
-    for program_header in elf_file.program_iter() {
-        debug!("SEGMENT {:?}", program_header);
-    }
-
-    for section in elf_file.section_iter() {
-        if wrap_res(section.get_type())? != ShType::Null {
-            debug!(
-                "SECTION name={} {:?}",
-                section.get_name(&elf_file).unwrap(),
-                section
-            );
-        }
-    }
-
-    resolve_dependencies(&elf_file)?;
+    load_segments(&elf_file)?;
 
     Ok(())
 }
 
-fn resolve_dependencies(elf_file: &ElfFile) -> Result<bool, LoaderError> {
-    let section = {
-        if let Some(section) = elf_file.find_section_by_name(".dynamic") {
-            section
-        } else {
-            return Ok(false);
+fn load_segments(elf_file: &ElfFile) -> Result<(), LoaderError> {
+    for program_header in elf_file.program_iter() {
+        if !matches!(program_header.get_type()?, program::Type::Load) {
+            continue;
         }
-    };
 
-    let data = {
-        if let SectionData::Dynamic64(data) = wrap_res(section.get_data(&elf_file))? {
-            data
-        } else {
-            return Err(LoaderError::BadDynamicSection);
-        }
-    };
+        let vaddr = program_header.virtual_addr() as usize;
+        let paddr = program_header.physical_addr() as usize;
+        let memsz = program_header.mem_size() as usize;
+        let filesz = program_header.file_size() as usize;
 
-    for entry in data {
-        match wrap_res(entry.get_tag())? {
-            xmas_elf::dynamic::Tag::Null => {
-                // this mark the end of the section
-                break;
-            }
+        debug!(
+            "Loading segment: paddr={:#x}, memsz={:#x}, filesz={:#x}, flags={:?}",
+            paddr,
+            memsz,
+            filesz,
+            program_header.flags()
+        );
 
-            xmas_elf::dynamic::Tag::Needed => {
-                // dynamic library to load
-                let str = wrap_res(elf_file.get_dyn_string(wrap_res(entry.get_val())? as u32))?;
-                debug!("NEEDED: {str}");
-            }
-            _ => {
-                // process later
-                debug!(".dynamic entry: {:?}", entry);
-            }
-        };
+        assert!(program_header.align() as usize >= kobject::PAGE_SIZE);
+
+        
+        // TODO: align addresses
+
+        assert!(vaddr % kobject::PAGE_SIZE == 0);
+        assert!(memsz % kobject::PAGE_SIZE == 0);
+
+        let mem_obj = kobject::MemoryObject::create(memsz)?;
+        let process = kobject::Process::current();
+        let mapping = process.map_mem(
+            None,
+            memsz,
+            kobject::Permissions::READ | kobject::Permissions::WRITE,
+            &mem_obj,
+            0,
+        )?;
+        let data = unsafe { mapping.as_buffer_mut().expect("Buffer not writable") };
     }
-    Ok(true)
-}
 
-fn load_segments(elf_file: &ElfFile) {}
-
-fn wrap_res<T>(res: Result<T, &'static str>) -> Result<T, LoaderError> {
-    res.map_err(|str| LoaderError::ElfReaderError(str))
+    panic!("unimplemented");
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum LoaderError {
     ElfReaderError(&'static str),
     BadObjectType(&'static str),
-    BadDynamicSection,
+    Error(kobject::Error),
+}
+
+impl From<kobject::Error> for LoaderError {
+    fn from(err: kobject::Error) -> LoaderError {
+        LoaderError::Error(err)
+    }
+}
+
+impl From<&'static str> for LoaderError {
+    fn from(err: &'static str) -> LoaderError {
+        LoaderError::ElfReaderError(err)
+    }
 }
 
 impl fmt::Display for LoaderError {
@@ -106,9 +98,7 @@ impl fmt::Display for LoaderError {
             LoaderError::BadObjectType(typ) => {
                 write!(formatter, "bad object type: '{}'", typ)
             }
-            LoaderError::BadDynamicSection => {
-                write!(formatter, "bad dynamic section")
-            }
+            LoaderError::Error(ref err) => write!(formatter, "loader error: {:?}", err),
         }
     }
 }

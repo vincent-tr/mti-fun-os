95b83e34e721c878c58c6e4af0bd3091dfc539bf
vincent-tr <vincent.tr@gmail.com>
2026-01-28 18:01:31 +0100

    first buffer passing

--- servers/process-server/src/main.rs
+++ servers/process-server/src/main.rs
@@ -6,9 +6,15 @@
 extern crate alloc;
 extern crate libruntime;
 
+use core::{mem, ops::Range};
+
 use log::{error, info};
 
-use libruntime::{ipc, process::messages};
+use libruntime::{
+    ipc,
+    kobject::{self, KObject, Permissions},
+    process::messages,
+};
 
 #[no_mangle]
 pub fn main() {
@@ -24,7 +30,66 @@ pub fn main() {
 
 fn create_process(
     query: messages::CreateProcessQueryParameters,
-    handles: ipc::Handles,
+    mut query_handles: ipc::Handles,
 ) -> Result<(messages::CreateProcessReply, ipc::Handles), messages::ProcessServerError> {
+    let mut name_handle = kobject::Handle::invalid();
+    mem::swap(&mut name_handle, &mut query_handles[1]);
+
+    let name_reader =
+        BufferReader::new(name_handle, &query.name).expect("failed to create buffer reader");
+    let str = unsafe { str::from_utf8_unchecked(name_reader.buffer()) };
+
+    info!("Creating process {}", str);
+
     Err(messages::ProcessServerError::InvalidArgument)
+}
+
+struct BufferReader {
+    mapping: kobject::Mapping<'static>,
+    range: Range<usize>,
+}
+
+impl BufferReader {
+    pub fn new(handle: kobject::Handle, buffer: &messages::Buffer) -> Result<Self, kobject::Error> {
+        let mem_obj = kobject::MemoryObject::from_handle(handle)?;
+
+        let process = kobject::Process::current();
+
+        // align mapping to page boundaries
+        let buffer_begin = buffer.offset;
+        let buffer_end = buffer.offset + buffer.size;
+        let mapping_begin = align_down(buffer_begin, kobject::PAGE_SIZE);
+        let mapping_end = align_up(buffer_end, kobject::PAGE_SIZE);
+        let mapping_size = mapping_end - mapping_begin;
+
+        let mapping = process.map_mem(
+            None,
+            mapping_size,
+            Permissions::READ,
+            &mem_obj,
+            mapping_begin,
+        )?;
+
+        let range_begin = buffer_begin - mapping_begin;
+        let range_end = range_begin + buffer.size;
+
+        Ok(Self {
+            mapping,
+            range: range_begin..range_end,
+        })
+    }
+
+    pub fn buffer(&self) -> &[u8] {
+        unsafe { self.mapping.as_buffer() }.expect("failed to get buffer")[self.range.clone()]
+            .as_ref()
+    }
+}
+
+// FIXME: factorize
+fn align_up(addr: usize, align: usize) -> usize {
+    (addr + align - 1) & !(align - 1)
+}
+
+fn align_down(addr: usize, align: usize) -> usize {
+    addr & !(align - 1)
 }

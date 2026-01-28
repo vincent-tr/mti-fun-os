9223388acf3a47d586149a731dc84e8f9bfaad60
vincent-tr <vincent.tr@gmail.com>
2026-01-28 11:17:54 +0100

    Add ipc client

---
+++ libs/libruntime/src/ipc/client.rs
@@ -0,0 +1,129 @@
+use core::{
+    fmt,
+    sync::atomic::{AtomicU32, Ordering},
+};
+
+use super::messages::{
+    Handles, QueryHeader, QueryMessage, ReplyErrorMessage, ReplyHeader, ReplySuccessMessage,
+};
+use crate::kobject::{self, KObject};
+
+/// IPC client error.
+#[derive(Debug)]
+pub enum CallError<ReplyError: fmt::Display + fmt::Debug> {
+    KernelError(kobject::Error),
+    ReplyError(ReplyError),
+}
+
+impl<ReplyError: fmt::Display + fmt::Debug> From<kobject::Error> for CallError<ReplyError> {
+    fn from(err: kobject::Error) -> Self {
+        CallError::KernelError(err)
+    }
+}
+
+impl<ReplyError: fmt::Display + fmt::Debug> fmt::Display for CallError<ReplyError> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            CallError::KernelError(err) => write!(f, "KernelError: {}", err),
+            CallError::ReplyError(err) => write!(f, "ReplyError: {}", err),
+        }
+    }
+}
+
+impl<ReplyError: fmt::Display + fmt::Debug> core::error::Error for CallError<ReplyError> {}
+
+/// IPC client.
+#[derive(Debug)]
+pub struct Client {
+    name: &'static str,
+    version: u16,
+    transaction_gen: AtomicU32,
+}
+
+impl Client {
+    /// Creates a new IPC client connected to the server with the given name and version.
+    pub fn new(name: &'static str, version: u16) -> Self {
+        Self {
+            name,
+            version,
+            transaction_gen: AtomicU32::new(1),
+        }
+    }
+
+    /// Calls a message on the server and waits for a reply.
+    pub fn call<MessageType, QueryParameters, ReplyContent, ReplyError>(
+        &self,
+        message_type: MessageType,
+        query: QueryParameters,
+        mut query_handles: Handles, // Note: first handle will be overwritten for reply port
+    ) -> Result<(ReplyContent, Handles), CallError<ReplyError>>
+    where
+        MessageType: Into<u16>,
+        QueryParameters: Copy,
+        ReplyContent: Copy,
+        ReplyError: Copy + fmt::Display + fmt::Debug,
+    {
+        let port = kobject::Port::open_by_name(self.name)?;
+        let (reply_reader, reply_sender) = kobject::Port::create(None)?;
+        let transaction = self.transaction_gen.fetch_add(1, Ordering::SeqCst);
+
+        query_handles[0] = reply_sender.into_handle();
+
+        let data = QueryMessage::<QueryParameters> {
+            header: QueryHeader {
+                version: self.version,
+                r#type: message_type.into(),
+                transaction,
+            },
+            parameters: query,
+        };
+
+        let mut message = unsafe { kobject::Message::new(&data, query_handles.as_mut_slice()) };
+
+        port.send(&mut message)?;
+
+        // TODO: add a timeout, if server dies during the query we will block forever here
+        let mut reply = reply_reader.blocking_receive()?;
+
+        let header = unsafe { reply.data::<ReplyHeader>() };
+        assert!(header.transaction == transaction);
+
+        if header.success {
+            let handles = reply.take_all_handles();
+            let reply_message = unsafe { reply.data::<ReplySuccessMessage<ReplyContent>>() };
+            Ok((reply_message.content, handles))
+        } else {
+            let error_message = unsafe { reply.data::<ReplyErrorMessage<ReplyError>>() };
+            Err(CallError::ReplyError(error_message.error))
+        }
+    }
+
+    /// Emits a message to the server without waiting for a reply.
+    pub fn emit<MessageType, QueryParameters>(
+        &self,
+        message_type: MessageType,
+        query: QueryParameters,
+        mut query_handles: Handles,
+    ) -> Result<(), kobject::Error>
+    where
+        MessageType: Into<u16>,
+        QueryParameters: Copy,
+    {
+        let port = kobject::Port::open_by_name(self.name)?;
+
+        let data = QueryMessage::<QueryParameters> {
+            header: QueryHeader {
+                version: self.version,
+                r#type: message_type.into(),
+                transaction: 0, // No transaction for emit
+            },
+            parameters: query,
+        };
+
+        let mut message = unsafe { kobject::Message::new(&data, query_handles.as_mut_slice()) };
+
+        port.send(&mut message)?;
+
+        Ok(())
+    }
+}
--- libs/libruntime/src/ipc/mod.rs
+++ libs/libruntime/src/ipc/mod.rs
@@ -1,5 +1,7 @@
+mod client;
 mod messages;
 mod server;
 
+pub use client::{CallError, Client};
 pub use messages::Handles;
 pub use server::{Server, ServerBuilder};

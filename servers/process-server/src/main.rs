bc83fe19dd568634f84c58646db0db1b97356d08
vincent-tr <vincent.tr@gmail.com>
2026-01-31 16:17:16 +0100

    wip process server

--- servers/process-server/src/main.rs
+++ servers/process-server/src/main.rs
@@ -6,7 +6,8 @@
 extern crate alloc;
 extern crate libruntime;
 
-use core::{mem, ops::Range};
+use core::ops::Range;
+use hashbrown::HashMap;
 
 use log::{error, info};
 
@@ -14,8 +15,71 @@ use libruntime::{
     ipc,
     kobject::{self, KObject, Permissions},
     memory,
-    process::messages,
+    process::{messages, KVBlock},
+    sync::{spin::OnceLock, RwLock},
 };
+
+lazy_static::lazy_static! {
+    static ref PROCESSES: RwLock<HashMap<Pid, ProcessInfo>> = RwLock::new(HashMap::new());
+}
+
+/// Process ID
+#[derive(Debug)]
+struct Pid(u64);
+
+/// Process information stored in the server
+#[derive(Debug)]
+struct ProcessInfo {
+    process: kobject::Process,
+    main_thread: kobject::Thread,
+    environment: kobject::MemoryObject,
+    arguments: kobject::MemoryObject,
+    exit_code: Option<i32>,
+}
+
+fn get_empty_kvblock() -> kobject::MemoryObject {
+    /// Since kvblocks are immutable, we can cache an empty one
+    static EMPTY_KVBLOCK: OnceLock<kobject::MemoryObject> = OnceLock::new();
+
+    let mobj = EMPTY_KVBLOCK.get_or_init(|| KVBlock::build(&[]));
+    mobj.clone()
+}
+
+/// Register the process-server itself in the system
+fn register_self() -> Result<(), kobject::Error> {
+    let process = kobject::Process::current().clone();
+    let pid = process.pid();
+    let main_thread = kobject::Thread::open_self()?;
+
+    let info = ProcessInfo {
+        process,
+        main_thread,
+        environment: get_empty_kvblock(),
+        arguments: get_empty_kvblock(),
+        exit_code: None,
+    };
+
+    Ok(())
+}
+
+fn register_init() -> Result<(), kobject::Error> {
+    const INIT_PID: u64 = 1;
+    // Note: this is fishy, we should really find the main thread differently
+    const INIT_MAIN_THREAD_TID: u64 = 3;
+
+    let process = kobject::Process::open(INIT_PID)?;
+    let main_thread = kobject::Thread::open(INIT_MAIN_THREAD_TID)?;
+
+    let info = ProcessInfo {
+        process,
+        main_thread,
+        environment: get_empty_kvblock(),
+        arguments: get_empty_kvblock(),
+        exit_code: None,
+    };
+
+    Ok(())
+}
 
 #[no_mangle]
 pub fn main() {
@@ -33,8 +97,7 @@ fn create_process(
     query: messages::CreateProcessQueryParameters,
     mut query_handles: ipc::Handles,
 ) -> Result<(messages::CreateProcessReply, ipc::Handles), messages::ProcessServerError> {
-    let mut name_handle = kobject::Handle::invalid();
-    mem::swap(&mut name_handle, &mut query_handles[1]);
+    let name_handle = query_handles.take(messages::CreateProcessQueryParameters::HANDLE_NAME_MOBJ);
 
     let name_reader =
         BufferReader::new(name_handle, &query.name).expect("failed to create buffer reader");

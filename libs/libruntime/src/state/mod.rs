152e4db3392e2978ebd643635679c455f11c81ce
vincent-tr <vincent.tr@gmail.com>
2026-02-05 11:36:01 +0100

    cleanup

--- libs/libruntime/src/state/mod.rs
+++ libs/libruntime/src/state/mod.rs
@@ -7,8 +7,6 @@ lazy_static::lazy_static! {
     static ref CLIENT: client::Client = client::Client::new();
 }
 
-type StateServerError = ipc::CallError<messages::StateServerError>;
-
 pub fn get_state(name: &str) -> ipc::BufferView {
     let mobj = CLIENT.get_state(name).expect("failed to get state");
 

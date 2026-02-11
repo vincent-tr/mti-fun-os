2200d814dc882af6969f639441d4b698c262f427
vincent-tr <vincent.tr@gmail.com>
2026-02-11 10:55:44 +0100

    Add async_sleep

--- libs/libruntime/src/timer/mod.rs
+++ libs/libruntime/src/timer/mod.rs
@@ -15,3 +15,26 @@ pub fn sleep(duration: Duration) {
         .blocking_receive()
         .expect("failed to receive timer event");
 }
+
+pub async fn async_sleep(duration: Duration) {
+    let now = kobject::Timer::now().expect("failed to get current time");
+    let timer = kobject::Timer::create(0).expect("failed to create timer");
+    timer
+        .arm(now + duration.as_u64())
+        .expect("failed to arm timer");
+
+    let res = loop {
+        crate::r#async::wait(&timer).await;
+
+        match timer.receive() {
+            Err(crate::kobject::Error::ObjectNotReady) => {
+                continue;
+            }
+            other => {
+                break other;
+            }
+        }
+    };
+
+    res.expect("failed to receive timer event");
+}

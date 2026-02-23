a83fc2643db25fbe83ccdcedf7dc946696b62ca8
vincent-tr <vincent.tr@gmail.com>
2026-02-23 17:25:24 +0100

    split init entry

---
+++ init/src/entry.rs
@@ -0,0 +1,111 @@
+use core::{arch::naked_asm, hint::unreachable_unchecked, mem::size_of, ops::Range};
+
+use crate::{main, offsets};
+use alloc::boxed::Box;
+use libruntime::{
+    kobject::{self, PAGE_SIZE, Permissions, ThreadOptions},
+    memory::align_up,
+};
+use log::debug;
+
+// Special init start: need to setup its own stack
+#[unsafe(naked)]
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".text_entry")]
+pub unsafe extern "C" fn user_start() -> ! {
+    naked_asm!(
+        "
+        lea rsp, {stack}
+
+        call {entry}
+        # `entry` must never return.
+        ud2
+        ",
+        stack = sym offsets::__init_stack_end,
+        entry = sym entry,
+    );
+}
+
+extern "C" fn entry(init_info_ptr: usize) -> ! {
+    libruntime::init();
+
+    let init_info = unsafe { &*(init_info_ptr as *const syscalls::init::InitInfo) };
+
+    apply_memory_protections(init_info.init_mapping.mapping_size);
+
+    let info = unsafe { load_init_info(init_info) };
+
+    // Jump to a safer thread, with better stack
+    let mut options = ThreadOptions::default();
+    options.name("main");
+    kobject::Thread::start(|| main(info), options).expect("Could not start main thread");
+
+    libsyscalls::thread::exit().expect("Failed to exit thread");
+    unsafe { unreachable_unchecked() };
+}
+
+fn apply_memory_protections(binary_len: usize) {
+    setup_protection(
+        "text",
+        offsets::text(),
+        Permissions::READ | Permissions::EXECUTE,
+    );
+
+    setup_protection("rodata", offsets::rodata(), Permissions::READ);
+
+    setup_protection(
+        "data",
+        offsets::data(),
+        Permissions::READ | Permissions::WRITE,
+    );
+
+    let unmapped_range_start = offsets::global().end;
+    let unmapped_range_end = offsets::global().start + binary_len;
+    // Align
+    let unmapped_range =
+        unmapped_range_start..(((unmapped_range_end + PAGE_SIZE - 1) / PAGE_SIZE) * PAGE_SIZE);
+
+    setup_protection("unmapped", unmapped_range, Permissions::READ);
+
+    fn setup_protection(name: &str, range: Range<usize>, perms: Permissions) {
+        // kernel has mapped one area with all permissions set
+        let initial_perms = Permissions::READ | Permissions::WRITE | Permissions::EXECUTE;
+        let process = kobject::Process::current();
+
+        unsafe {
+            let mut mapping = kobject::Mapping::unleak(process, range.clone(), initial_perms);
+            let res = mapping.update_permissions(perms);
+            mapping.leak(); // be sure to not drop the mapping, even on error, else we we have troubes to show the panic
+            res.expect("Could not setup memory protection");
+        }
+
+        debug!(
+            "{}: 0x{:016X} -> 0x{:016X} (size=0x{:X})",
+            name,
+            range.start,
+            range.end,
+            range.len()
+        );
+    }
+}
+
+/// Safety: do not use reference to init info after this, as the memory used by the kernel to pass it will be released.
+unsafe fn load_init_info(init_info: &syscalls::init::InitInfo) -> Box<syscalls::init::InitInfo> {
+    let data = Box::new(init_info.clone());
+
+    // Release the memory used by the kernel to pass the init info, as we have copied it
+    let addr = init_info as *const syscalls::init::InitInfo as usize;
+    assert!(addr % PAGE_SIZE == 0);
+    let size = align_up(size_of::<syscalls::init::InitInfo>(), PAGE_SIZE);
+
+    unsafe {
+        kobject::Mapping::unleak(
+            kobject::Process::current(),
+            addr..addr + size,
+            Permissions::READ,
+        )
+    }
+    .leak();
+
+    data
+}
--- init/src/main.rs
+++ init/src/main.rs
@@ -5,67 +5,20 @@
 extern crate alloc;
 
 mod archive;
+mod entry;
 mod idle;
 mod loader;
 mod offsets;
 mod state_server;
 mod tests;
 
-use core::{arch::naked_asm, hint::unreachable_unchecked, mem::size_of, ops::Range};
-
 use alloc::boxed::Box;
-use libruntime::{
-    ipc,
-    kobject::{self, PAGE_SIZE, Permissions, ThreadOptions},
-    memory::align_up,
-    process, state,
-    sync::spin::OnceLock,
-    vfs,
-};
+use libruntime::{ipc, kobject, process, state, vfs};
 use log::{debug, info};
 
-/// Init info provided by the kernel at startup, it contains information about the initial mapping of the init process
-static INIT_INFO: OnceLock<syscalls::init::InitInfo> = OnceLock::new();
+fn main(info: Box<syscalls::init::InitInfo>) {
+    debug!("Init info: {:#?}", info);
 
-// Special init start: need to setup its own stack
-#[unsafe(naked)]
-#[unsafe(no_mangle)]
-#[unsafe(link_section = ".text_entry")]
-pub unsafe extern "C" fn user_start() -> ! {
-    naked_asm!(
-        "
-        lea rsp, {stack}
-
-        call {entry}
-        # `entry` must never return.
-        ud2
-        ",
-        stack = sym offsets::__init_stack_end,
-        entry = sym entry,
-    );
-}
-
-extern "C" fn entry(init_info_ptr: usize) -> ! {
-    // Get init info
-    let init_info = unsafe { &*(init_info_ptr as *const syscalls::init::InitInfo) };
-
-    // let binary = unsafe { slice::from_raw_parts(offsets::global().start as *const u8, binary_len) };
-    libruntime::init();
-
-    apply_memory_protections(init_info.init_mapping.mapping_size);
-
-    unsafe { load_init_info(init_info) };
-
-    // Jump to a safer thread, with better stack
-    let mut options = ThreadOptions::default();
-    options.name("main");
-    kobject::Thread::start(main, options).expect("Could not start main thread");
-
-    libsyscalls::thread::exit().expect("Failed to exit thread");
-    unsafe { unreachable_unchecked() };
-}
-
-fn main() {
     idle::create_idle_process().expect("Could not create idle process");
 
     // tests::thread::dump_processes_threads();
@@ -154,72 +107,6 @@ fn setup_initial_filesystem() {
             mount.fs_port_name, mount.mount_point
         );
     }
-}
-
-fn apply_memory_protections(binary_len: usize) {
-    setup_protection(
-        "text",
-        offsets::text(),
-        Permissions::READ | Permissions::EXECUTE,
-    );
-
-    setup_protection("rodata", offsets::rodata(), Permissions::READ);
-
-    setup_protection(
-        "data",
-        offsets::data(),
-        Permissions::READ | Permissions::WRITE,
-    );
-
-    let unmapped_range_start = offsets::global().end;
-    let unmapped_range_end = offsets::global().start + binary_len;
-    // Align
-    let unmapped_range =
-        unmapped_range_start..(((unmapped_range_end + PAGE_SIZE - 1) / PAGE_SIZE) * PAGE_SIZE);
-
-    setup_protection("unmapped", unmapped_range, Permissions::READ);
-
-    fn setup_protection(name: &str, range: Range<usize>, perms: Permissions) {
-        // kernel has mapped one area with all permissions set
-        let initial_perms = Permissions::READ | Permissions::WRITE | Permissions::EXECUTE;
-        let process = kobject::Process::current();
-
-        unsafe {
-            let mut mapping = kobject::Mapping::unleak(process, range.clone(), initial_perms);
-            let res = mapping.update_permissions(perms);
-            mapping.leak(); // be sure to not drop the mapping, even on error, else we we have troubes to show the panic
-            res.expect("Could not setup memory protection");
-        }
-
-        debug!(
-            "{}: 0x{:016X} -> 0x{:016X} (size=0x{:X})",
-            name,
-            range.start,
-            range.end,
-            range.len()
-        );
-    }
-}
-
-/// Safety: do not use reference to init info after this, as the memory used by the kernel to pass it will be released.
-unsafe fn load_init_info(init_info: &syscalls::init::InitInfo) {
-    INIT_INFO
-        .set(init_info.clone())
-        .expect("Could not set init info");
-
-    // Release the memory used by the kernel to pass the init info, as we have copied it
-    let addr = init_info as *const syscalls::init::InitInfo as usize;
-    assert!(addr % PAGE_SIZE == 0);
-    let size = align_up(size_of::<syscalls::init::InitInfo>(), PAGE_SIZE);
-
-    unsafe {
-        kobject::Mapping::unleak(
-            kobject::Process::current(),
-            addr..addr + size,
-            Permissions::READ,
-        )
-    }
-    .leak();
 }
 
 /// Wait for a server port to be available

a57fe4b0b3692df5fa2d54605996460105b885df
vincent-tr <vincent.tr@gmail.com>
2026-02-28 18:11:45 +0100

    wip pci scan

--- servers/pci-server/src/main.rs
+++ servers/pci-server/src/main.rs
@@ -1,18 +1,36 @@
 #![no_std]
 #![no_main]
 
-use log::info;
-
 extern crate alloc;
 extern crate libruntime;
+
+mod pci;
+
+use log::info;
+use pci::{PciClass, PciConfig, PciDevice};
 
 #[unsafe(no_mangle)]
 pub fn main() -> i32 {
     info!("PCI server started");
 
-    // TODO: Implement PCI device enumeration and management
+    // https://wiki.osdev.org/PCI
 
-    info!("PCI server initialized");
+    let pci = PciConfig::open();
+
+    let devices = pci.scan_bus(0);
+    for device in devices {
+        let id = pci.get_id(device);
+        let class = pci.get_class(device);
+
+        info!(
+            "Found PCI device: bus {}, device {}, function {}, id {}, class {:?}",
+            device.bus(),
+            device.device(),
+            device.function(),
+            id,
+            class
+        );
+    }
 
     0
 }

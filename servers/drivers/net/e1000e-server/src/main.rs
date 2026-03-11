650e2c86f4a5e64dd791edc11b41188046508653
vincent-tr <vincent.tr@gmail.com>
2026-03-11 21:44:28 +0100

    cleanup

--- servers/drivers/net/e1000e-server/src/main.rs
+++ servers/drivers/net/e1000e-server/src/main.rs
@@ -13,12 +13,6 @@ use device::E1000eDevice;
 
 #[unsafe(no_mangle)]
 pub fn main() -> i32 {
-    // 8086:10d3
-
-    // TODO:
-    // netdev interface
-    // init flow (PCI probing, device initialization, net stack registration)
-
     let ipc_server = build_net_device_server::<E1000eDevice>("net.dev.e1000e")
         .expect("failed to build net.dev.e1000e IPC server");
 

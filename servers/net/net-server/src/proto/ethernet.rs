7acb75389ced396693a8f9083edaf78a71fce231
vincent-tr <vincent.tr@gmail.com>
2026-04-13 13:17:24 +0200

    WIP ethernet

--- servers/net/net-server/src/proto/ethernet.rs
+++ servers/net/net-server/src/proto/ethernet.rs
@@ -2,7 +2,7 @@ use core::mem;
 
 use crc::{CRC_32_ISO_HDLC, Crc};
 use libruntime::{debug, net::types::MacAddress};
-use log::{debug, error};
+use log::{debug, warn};
 
 use crate::packet::{Packet, PacketCursor};
 
@@ -17,14 +17,20 @@ struct EthernetHeader {
     ethertype: NetU16,
 }
 
+/// Ethernet footer struct, representing the footer of an Ethernet frame.
+#[derive(Debug)]
+#[repr(packed)]
 struct EthernetFooter {
     checksum: NetU32,
 }
 
+const ETHERTYPE_IPV4: u16 = 0x0800;
+const ETHERTYPE_ARP: u16 = 0x0806;
+
 /// Process an incoming Ethernet frame, given the raw bytes of the frame.
 pub async fn rx_packet(packet: Packet) {
     if packet.len() < mem::size_of::<EthernetHeader>() + mem::size_of::<EthernetFooter>() {
-        error!(
+        warn!(
             "Received packet too short to contain Ethernet header and footer: length={} (dropped)",
             packet.len()
         );
@@ -54,16 +60,40 @@ pub async fn rx_packet(packet: Packet) {
     let checksum_payload = packet.slice(0..packet.len() - mem::size_of::<EthernetFooter>());
     let checksum = compute_checksum(&checksum_payload);
 
-    if checksum != footer.checksum.to_u32() {
-        error!(
-            "Received packet with invalid checksum: expected={:#010x}, actual={:#010x} (dropped)",
-            footer.checksum.to_u32(),
-            checksum
-        );
-        return;
-    }
+    // Note: QEmu does not provide FCS value in the Ethernet footer, so we cannot actually verify the checksum here.
+    let _ = checksum;
+    let _ = footer.checksum.to_u32();
+    // if checksum != footer.checksum.to_u32() {
+    //     warn!(
+    //         "Received packet with invalid checksum: expected={:#010x}, actual={:#010x} (dropped)",
+    //         footer.checksum.to_u32(),
+    //         checksum
+    //     );
+    //     return;
+    // }
 
-    // TODO: process payload based on ethertype in header
+    match header.ethertype.to_u16() {
+        ETHERTYPE_IPV4 => {
+            // TODO
+            debug!(
+                "Received IPv4 packet from {} to {}",
+                header.source, header.destination
+            );
+        }
+        ETHERTYPE_ARP => {
+            // TODO
+            debug!(
+                "Received ARP packet from {} to {}",
+                header.source, header.destination
+            );
+        }
+        ethertype => {
+            warn!(
+                "Received packet with unknown ethertype {:#06x} from {} to {} (dropped)",
+                ethertype, header.source, header.destination
+            );
+        }
+    }
 }
 
 /// Computes the CRC32 checksum of the given packet data, using the same algorithm as used by Ethernet frames.
